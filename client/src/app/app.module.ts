6e4589b547ceba914765514215ef0a050cf9b7fa
JAGADESH <jagadesh@JAGADESHs-MacBook-Air.local>
2018-02-27 19:35:30 +0530

    27th feb Rocky test 2

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -44,7 +44,7 @@ const config = new AuthServiceConfig([
   },
   {
     id: FacebookLoginProvider.PROVIDER_ID,
-    provider: new FacebookLoginProvider('Facebook-App-Id')
+    provider: new FacebookLoginProvider('1997818163768814')
   }
 ]);
 
